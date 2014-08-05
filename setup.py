246018475be1c6bedff8471b9b2a30aa7f62d3f9
ozdan <ozdan@inbox.ru>
2014-08-05 18:39:15 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     description='Django simple poll application',
     author='Dmitry Akinin',
     author_email='d.akinin@gmail.com',
-    url='https://github.com/applecat/django-simple-poll',
-    download_url='https://github.com/applecat/django-simple-poll/tarball/0.1.1',
+    url='https://github.com/ozdan/django-simple-poll',
+    download_url='https://github.com/ozdan/django-simple-poll/tarball/0.1.1',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
